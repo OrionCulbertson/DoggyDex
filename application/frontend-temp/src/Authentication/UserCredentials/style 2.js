a9b8907db48578136da01d4e7e72ee9ed355493f
mgacoder <miguelgalvanalvarado@gmail.com>
2021-11-10 11:56:34 -0800

    removed deleted files from tracking

--- application/frontend-temp/src/Authentication/UserCredentials/style 2.js
+++
@@ -1,8 +0,0 @@
-// styles.js
-// npm install --save styled-components
-import styled from "styled-components";
-import  {NavLink}  from "react-router-dom";
-
-const StyledLink =  styled(NavLink)`
- margin-right: 25px`;
-export{StyledLink};
