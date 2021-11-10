5467b18c5677e8490ee1f86c94b5eb2f5dbe1629
OrionCulbertson <oculbertson@mail.sfsu.edu>
2021-11-09 16:38:51 -0800

    Added Redux User Session

--- application/client-end/current-client/src/App.js
+++ application/client-end/current-client/src/App.js
@@ -3,29 +3,26 @@ import UploadDog from "./components/UploadDog";
 import Menu from "./components/Menu";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import About from "./components/About";
-// import FileUpload from "./components/FileUpload";
 import UserProfile from "./components/UserProfile";
+import { Provider } from "react-redux";
+import store from "./store";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Menu />
-        <Route path="/" exact render={(props) => (
-          <>
-            <Logo />
-            <div className="contentContainer">
-              <UploadDog />
-            </div>
-          </>
-        )} />
-        <Route path="/about" component={About} />
-        <Route path="/user-profile" component={UserProfile} />
-        <footer>
-          <Link to="/about">About the Devs</Link>
-        </footer>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Menu />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/user-profile" component={UserProfile} />
+          <footer>
+            <Link to="/about">About the Devs</Link>
+          </footer>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ application/client-end/current-client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './Logo'
+import UploadDog from './UploadDog'
+
+const Home = () => {
+    return (
+        <>
+            <Logo />
+            <div className="contentContainer">
+                <UploadDog />
+            </div>
+        </>
+    )
+}
+
+export default Home
