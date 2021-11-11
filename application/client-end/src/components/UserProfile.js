8a3c5cf9fc32ba2da9a32d5cb433b2d49ae47018
krisp1994 <6775270+krispy1994@users.noreply.github.com>
2021-11-11 15:59:18 -0800

    First draft of logged in state of UserProfile.js

--- application/client-end/src/components/UserProfile.js
+++ application/client-end/src/components/UserProfile.js
@@ -5,7 +5,7 @@ import Logo from './Logo';
 
 const UserProfile = () => {
     //Change to check global user state
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
 
     const loginOptions = [
         "Login",
@@ -21,30 +21,53 @@ const UserProfile = () => {
     return (
         <>
             <Logo />
-            <div className="contentContainer">
+            
             {isLoggedIn ?
-                <Link
-                    className="menuLink">
-                    onClick = { () => {
-                        isLoggedIn = false;
-                    }}
-                    <Button contents={<div>Log Out</div>} styleClass="stdButton" />
-                </Link>
+                <>
+                    <div className="contentContainer">
+                        <table>
+                            <tr className="userProfileTableLine">
+                                <td>Name:</td>
+                                <td>TODO</td>
+                            </tr>
+                            <tr className="userProfileTableLine">
+                                <td>Username:</td>
+                                <td>TODO</td>
+                            </tr>
+                            <tr className="userProfileTableLine">
+                                <td>Email:</td>
+                                <td>TODO</td>
+                            </tr>
+                            <tr className="userProfileTableLine">
+                                <td>Dogs Found:</td>
+                                <td>TODO</td>
+                            </tr>
+                            <tr className="userProfileTableLine">
+                                <td>Dog Breeds Found:</td>
+                                <td>TODO</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <Link id="logOutButton" className="menuLink">
+                        <Button contents={<div>Log Out</div>} id="logOutButton" styleClass="stdButton" />
+                    </Link>
+                </>
 
                 :
 
                 <>
-                    {loginOptions.map((option, index) => (
-                        <div>
-                            <Link key={index} to={loginLinks[option]} className="menuLink">
-                                <Button contents={option} styleClass="stdButton" />
-                            </Link>
-                            {/* <Button contents={<Link to={loginLinks[option]} className="menuLink">{option}</Link>} styleClass="stdButton" /> */}
-                        </div>
-                    ))}
+                    <div className="contentContainer">
+                        {loginOptions.map((option, index) => (
+                            <div>
+                                <Link key={index} to={loginLinks[option]} className="menuLink">
+                                    <Button contents={option} styleClass="stdButton" />
+                                </Link>
+                                {/* <Button contents={<Link to={loginLinks[option]} className="menuLink">{option}</Link>} styleClass="stdButton" /> */}
+                            </div>
+                        ))}
+                    </div>
                 </>
             }
-            </div>
         </>
     )
 }
