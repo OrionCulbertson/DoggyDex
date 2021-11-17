8b35849d8a0fedd07d9fc0064f12018067864c52
OrionCulbertson <oculbertson@mail.sfsu.edu>
2021-11-16 17:08:13 -0800

    Connected Photo Upload FE and BE

--- application/client-end/src/components/DogSubmission.js
+++ application/client-end/src/components/DogSubmission.js
@@ -1,67 +1,100 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { FaArrowUp, FaPaw } from "react-icons/fa";
 import Button from './Button';
 import axios from 'axios';
 
 
-const DogSubmission = ({setDogUploaded, setIsDogUploaded, getDogInfo}) => {
-    const [file, setFile] = useState("");
-    const [fileName, setFileName] = useState("Choose File");
-    const [uploadedFile, setUploadedFile] = useState({});
-    const [message, setMessage] = useState("");
+const DogSubmission = ({ setDogUploaded, setIsDogUploaded, getDogInfo }) => {
+  const [file, setFile] = useState("");
+  const [fileName, setFileName] = useState("Choose File");
+  const [uploadedFile, setUploadedFile] = useState({});
+  const [message, setMessage] = useState("");
+  const [photo, setPhoto] = useState(null);
+  const [imgFile, setImgFile] = useState('');
 
-    const onChange = e => {
-        //TO-DO: we need to filter out non-image files
-        setFile(e.target.files[0]);
-        setFileName(e.target.files[0].name);
-    };
+  const [testImage, setTestImage] = useState(null);
+  const onChange = (event) => {
+    const file = event.target.files[0];
+    setPhoto(file);
+  };
+  // const onChange = e => {
+  //     //TO-DO: we need to filter out non-image files
+  //     setFile(e.target.files[0]);
+  //     setFileName(e.target.files[0].name);
+  // };
 
-    const onSubmit = async e => {
-        e.preventDefault();
-        const formData = new FormData();
-        formData.append("file", file);
-        // console.log(file);
-        try {
-            console.log(formData)
-            const res = await axios.post("/api/image/upload/", formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data'
-                }
-            });
-
-            const { fileName, filePath, dog_id, confidenceScore} = res.data;
-            setUploadedFile({ fileName, filePath, dog_id }); //May not need this
-            setMessage("File Uploaded");
-            setDogUploaded({dog_id, confidenceScore});
-            // setDogUploaded({dog_id: 10, confidenceScore: 90}); //Testing information
-            getDogInfo(dog_id); //Loads dog object based off dog breed
-            setIsDogUploaded(true); //Updates the page
-            setFile({}); //Reset File Upload info
-            setFileName(""); //Reset File Name info
-            //TODO: if user is logged in, store in user's profile
-            
-        } catch (err) {
-            console.log("error")
-            if (err.response.status === 500) {
-                setMessage("Server error");
-            } else {
-                setMessage(err.response.data.msg);
-            }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append('image', photo);
+      axios.post('http://localhost:8080/api/image/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
         }
-    };
-    
-    return (
-        <>
-            <p className="bigText">Upload A Dog Picture!</p>
-            <form onSubmit={onSubmit}>
-                <label htmlFor="dogUpload" className="uploadDogButton">
-                    <i>{<FaPaw />}</i> Select Dog Photo... <i>{<FaPaw />}</i>
-                </label>
-                <input id="dogUpload" type="file" onChange={onChange} />
-                <Button contents={<div>Submit <FaArrowUp /></div>} styleClass="stdButton" type="submit" />
-            </form>
-        </>
-    )
+      })
+        .then(res => {
+          setImgFile('http://localhost:8080/api/image/id/' + res.data._id);
+          console.log(imgFile);
+          
+          // axios.get(imgFile).then(res => {
+          //   // setTestImage(res.data);
+          //   // console.log(res);
+          // })
+        })
+    } catch (error) {
+      error.response("Err" + error.response.data);
+    }
+  };
+  // const onSubmit = async e => {
+  //     e.preventDefault();
+  //     const formData = new FormData();
+  //     formData.append("file", file);
+  //     // console.log(file);
+  //     try {
+  //         console.log(formData)
+  //         const res = await axios.post("/api/image/upload/", formData, {
+  //             headers: {
+  //                 'Content-Type': 'multipart/form-data'
+  //             }
+  //         });
+
+  //         const { fileName, filePath, dog_id, confidenceScore} = res.data;
+  //         setUploadedFile({ fileName, filePath, dog_id }); //May not need this
+  //         setMessage("File Uploaded");
+  //         setDogUploaded({dog_id, confidenceScore});
+  //         // setDogUploaded({dog_id: 10, confidenceScore: 90}); //Testing information
+  //         getDogInfo(dog_id); //Loads dog object based off dog breed
+  //         setIsDogUploaded(true); //Updates the page
+  //         setFile({}); //Reset File Upload info
+  //         setFileName(""); //Reset File Name info
+  //         //TODO: if user is logged in, store in user's profile
+
+  //     } catch (err) {
+  //         console.log("error")
+  //         if (err.response.status === 500) {
+  //             setMessage("Server error");
+  //         } else {
+  //             setMessage(err.response.data.msg);
+  //         }
+  //     }
+  // };
+
+  return (
+    <>
+      <p className="bigText">Upload A Dog Picture!</p>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="dogUpload" className="uploadDogButton">
+          <i>{<FaPaw />}</i> Select Dog Photo... <i>{<FaPaw />}</i>
+        </label>
+        <input id="dogUpload" type="file" onChange={onChange} />
+        <Button contents={<div>Submit <FaArrowUp /></div>} styleClass="stdButton" type="submit" />
+        {imgFile && <img src={imgFile} style={{
+          width: "90%",
+        }}/>}
+      </form>
+    </>
+  )
 }
 
 export default DogSubmission;
@@ -88,7 +121,7 @@ const handleOnChange = (event) => {
             headers: {
               'Content-Type': 'multipart/form-data'
          }})
-          .then(res => { 
+          .then(res => {
             setImgFile('http://localhost:8080/api/image/id/' + res.data._id)
           })
       } catch (error) {
@@ -118,7 +151,7 @@ const handleOnChange = (event) => {
 
       </form>
       <div>
-      <img 
+      <img
       style={{ width: "25%", margin: "30px 0" }}
       src={imgFile} alt="img"/>
       </div>
