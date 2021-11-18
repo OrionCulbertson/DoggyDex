9f0a02d8c0c94a0fa7feeae2301c4f2ed2b4bb4e
Rigo P-Angeles <rigoangeles@MacBookLate13.local>
2021-11-17 23:06:37 -0800

    New field added to the user schema

--- application/backend/routes/api/basicUser.js
+++ application/backend/routes/api/basicUser.js
@@ -6,21 +6,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("../../config/auth.config");// Keeps secret key
 
-// function getHashedPassword(password){
-   
-//   const saltRounds = 10; 
-//   const hashedPassword = new Promise((resolve, reject) => {
-//     bcrypt.genSalt(saltRounds, function(err, salt){
-//       bcrypt.hash(password, salt, function(err, hash) {
-//         if(err) reject(err)
-//         resolve(hash)
-//       });
-//     });
-//   });
-//   return hashedPassword;
-// } in case we implement update user function will be necc. 
-
-
 
 // Load Basic_User model
 const BasicUser = require('../../models/Basic_User');
@@ -71,7 +56,7 @@ router.post('/signup', (req, res, next) => {
             user
               .save()
               .then(result => {
-                console.log(result);
+                console.log(result); // <---- ---- Remove when finishing dev
                 res.status(201).json({
                   message: "User successfully created"
                 });
@@ -89,19 +74,9 @@ router.post('/signup', (req, res, next) => {
     });
 });
 
-/*
-//Basic_User router
-router.post('/', (req, res) => {
-  BasicUser.create(req.body)
-  .then(user => res.json({ msg: 'BasicUser added successfully' }))
-  .catch(err => res.status(400).json({ error: 'Unable to add this user' }));  
-});
-*/
 // @route POST api/basicuser/login
 // @description Update basicuser
 router.post("/login", (req, res, next) => {
-
-
   BasicUser.findOne({ email: req.body.email })
     .exec()
     .then(user => {
@@ -118,19 +93,20 @@ router.post("/login", (req, res, next) => {
         }
 
         if (result) {
-           const token = jwt.sign({userName: user.userName, userId: user._id}, // Get any of the user's properties
-            config.secret,
+          const token = jwt.sign(
             {
-                expiresIn: "1h"
-            }
-          );
+             userName: user.userName,
+             userId: user._id,
+             dogbreedIDs: user.dogbreedIDs,
+           }, // Get any of the user's properties
+           config.secret,
+           {
+               expiresIn: "1h"
+           }
+         );
           return res.status(200).json({
             message: "Auth successful",
-            token: token,
-            name: user.name,
-            userName: user.userName,
-            email: user.email,
-            userId: user._id,
+            token: token
           });
         }
 
@@ -147,7 +123,6 @@ router.post("/login", (req, res, next) => {
     });
 });
 
-
 // @route GET api/basicuser/:id
 // @description Update basicuser
 router.put('/:id', (req, res) => {
@@ -158,6 +133,26 @@ router.put('/:id', (req, res) => {
     );
 });
 
+// find by document id and update and push item in array
+// @route PUT: user_id gets the account where a new DOGBREED ID will be insert
+// dog_id is the actual DOGBREED ID
+// Request: hhtp... /api/basicuser/add/<MongoDB user ID>/<dog id>
+
+
+router.put('/add/:user_id/:dog_id', (req, res) => {
+  BasicUser.findByIdAndUpdate(req.params.user_id,
+    {
+      $addToSet: 
+      {
+        dogbreedIDs: req.params.dog_id,
+      }
+    }
+  )
+  .then(user => res.json({ msg: 'Dogbreed ID added successfully' }))
+  .catch(err =>
+    res.status(400).json({ error: 'Unable to update  dogbreed ID' })
+  );
+});
 
 // @route GET api/basicuser/:id
 // @description Delete user by id
@@ -166,10 +161,6 @@ router.delete('/:id', (req, res) => {
     .then(user => res.json({ mgs: 'User entry deleted successfully' }))
     .catch(err => res.status(404).json({ error: 'No such a user found' }));
 });
-
-
-
-
 
 module.exports = router;
 
