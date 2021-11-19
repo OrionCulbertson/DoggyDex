a735b52f4170fcf4d3584b7536986ab9b03ebf12
OrionCulbertson <oculbertson@mail.sfsu.edu>
2021-11-18 18:56:27 -0800

    Fixed Backend info bugs

--- application/backend/routes/api/basicUser.js
+++ application/backend/routes/api/basicUser.js
@@ -1,14 +1,14 @@
 // routes/api/basicuser.js
 const express = require('express');
 const router = express.Router();
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const config = require("../../config/auth.config");// Keeps secret key
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../../config/auth.config'); // Keeps secret key
 
 // function getHashedPassword(password){
-   
-//   const saltRounds = 10; 
+
+//   const saltRounds = 10;
 //   const hashedPassword = new Promise((resolve, reject) => {
 //     bcrypt.genSalt(saltRounds, function(err, salt){
 //       bcrypt.hash(password, salt, function(err, hash) {
@@ -23,22 +23,22 @@ const config = require("../../config/auth.config");// Keeps secret key
 // Load Basic_User model
 const BasicUser = require('../../models/Basic_User');
 
-
 router.get('/test', (req, res) => res.send('routes route testing!'));
 
 // @route GET api/basicuser
 router.get('/', (req, res) => {
   BasicUser.find()
-    .then(user => res.json(user))
-    .catch(err => res.status(404).json({ nodogbreedfound: 'No user found' }));
+    .then((user) => res.json(user))
+    .catch((err) => res.status(404).json({ nodogbreedfound: 'No user found' }));
 });
 
 // @route GET api/basicuser/:id
 router.get('/:id', (req, res) => {
   BasicUser.findById(req.params.id)
-  .then(user => res.json(user))
-  .catch(err => res.status(404).json({ nouserfound: '***No User found***' }));
-
+    .then((user) => res.json(user))
+    .catch((err) =>
+      res.status(404).json({ nouserfound: '***No User found***' })
+    );
 });
 
 //Basic_User router
@@ -46,39 +46,38 @@ router.get('/:id', (req, res) => {
 router.post('/signup', (req, res, next) => {
   BasicUser.find({ email: req.body.email })
     .exec()
-    .then(user => {
+    .then((user) => {
       if (user.length >= 1) {
         return res.status(409).json({
-          message: "Email already exists"
+          message: 'Email already exists',
         });
       } else {
         bcrypt.hash(req.body.password, 10, (err, hash) => {
           if (err) {
             return res.status(500).json({
-              error: err
+              error: err,
             });
           } else {
-
             const user = new BasicUser({
               _id: new mongoose.Types.ObjectId(),
               name: req.body.name,
               userName: req.body.userName,
               email: req.body.email,
-              password: hash
+              password: hash,
             });
             user
               .save()
-              .then(result => {
+              .then((result) => {
                 console.log(result); // <---- ---- Remove when finishing dev
                 res.status(201).json({
-                  message: "User successfully created"
+                  message: 'User successfully created',
                 });
               })
-              .catch(err => {
+              .catch((err) => {
                 console.log(err);
 
                 res.status(500).json({
-                  error: err
+                  error: err,
                 });
               });
           }
@@ -89,36 +88,36 @@ router.post('/signup', (req, res, next) => {
 
 // @route POST api/basicuser/login
 // @description Update basicuser
-router.post("/login", (req, res, next) => {
+router.post('/login', (req, res, next) => {
   BasicUser.findOne({ email: req.body.email })
     .exec()
-    .then(user => {
+    .then((user) => {
       if (user.length < 1) {
         return res.status(401).json({
-          message: "Auth failed"
+          message: 'Auth failed',
         });
       }
       bcrypt.compare(req.body.password, user.password, (err, result) => {
         if (err) {
           return res.status(401).json({
-            message: "Auth failed. Check password and email."
+            message: 'Auth failed. Check password and email.',
           });
         }
 
         if (result) {
           const token = jwt.sign(
             {
-             userName: user.userName,
-             userId: user._id,
-             dogbreedIDs: user.dogbreedIDs,
-           }, // Get any of the user's properties
-           config.secret,
-           {
-               expiresIn: "1h"
-           }
-         );
+              userName: user.userName,
+              userId: user._id,
+              dogbreedIDs: user.dogbreedIDs,
+            }, // Get any of the user's properties
+            config.secret,
+            {
+              expiresIn: '1h',
+            }
+          );
           return res.status(200).json({
-            message: "Auth successful",
+            message: 'Auth successful',
             token: token,
             name: user.name,
             email: user.email,
@@ -126,14 +125,14 @@ router.post("/login", (req, res, next) => {
         }
 
         res.status(401).json({
-          message: "Auth failed"
+          message: 'Auth failed',
         });
       });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       res.status(403).json({
-        error: err
+        error: err,
       });
     });
 });
@@ -142,8 +141,8 @@ router.post("/login", (req, res, next) => {
 // @description Update basicuser
 router.put('/:id', (req, res) => {
   BasicUser.findByIdAndUpdate(req.params.id, req.body)
-    .then(user => res.json({ msg: 'Updated successfully' }))
-    .catch(err =>
+    .then((user) => res.json({ msg: 'Updated successfully' }))
+    .catch((err) =>
       res.status(400).json({ error: 'Unable to update the Database' })
     );
 });
@@ -153,30 +152,24 @@ router.put('/:id', (req, res) => {
 // dog_id is the actual DOGBREED ID
 // Request: hhtp... /api/basicuser/add/<MongoDB user ID>/<dog id>
 
-
 router.put('/add/:user_id/:dog_id', (req, res) => {
-  BasicUser.findByIdAndUpdate(req.params.user_id,
-    {
-      $addToSet: 
-      {
-        dogbreedIDs: req.params.dog_id,
-      }
-    }
-  )
-  .then(user => res.json({ msg: 'Dogbreed ID added successfully' }))
-  .catch(err =>
-    res.status(400).json({ error: 'Unable to update  dogbreed ID' })
-  );
+  BasicUser.findByIdAndUpdate(req.params.user_id, {
+    $addToSet: {
+      dogbreedIDs: req.params.dog_id,
+    },
+  })
+    .then((user) => res.json({ msg: 'Dogbreed ID added successfully' }))
+    .catch((err) =>
+      res.status(400).json({ error: 'Unable to update  dogbreed ID' })
+    );
 });
 
 // @route GET api/basicuser/:id
 // @description Delete user by id
 router.delete('/:id', (req, res) => {
   BasicUser.findByIdAndRemove(req.params.id, req.body)
-    .then(user => res.json({ mgs: 'User entry deleted successfully' }))
-    .catch(err => res.status(404).json({ error: 'No such a user found' }));
+    .then((user) => res.json({ mgs: 'User entry deleted successfully' }))
+    .catch((err) => res.status(404).json({ error: 'No such a user found' }));
 });
 
 module.exports = router;
-
-
--- application/client-end/src/components/DoggyDex.js
+++ application/client-end/src/components/DoggyDex.js
@@ -1,46 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { connect, useSelector } from 'react-redux';
-import { fetchDogs } from '../actions/dogActions';
-import { DoggyDexToggle, LearningModeDoggyDex } from '../components'
+import React, { useState, useEffect } from "react";
+import { connect, useSelector } from "react-redux";
+import { fetchDogs } from "../actions/dogActions";
+import { DoggyDexToggle, LearningModeDoggyDex } from "../components";
 
+function DoggyDex({ dogData, fetchDogs }) {
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
-function DoggyDex({ dogData, fetchDogs }) { 
-    
-    const {isLoggedIn} = useSelector(state => state.auth)
+  useEffect(() => {
+    fetchDogs();
+  }, []);
 
-
-
-    useEffect(() => {
-       
-        fetchDogs()
-    }, [] );
-
-    
-    return dogData.loading ? (
-        <h2>Loading</h2>
-       
-    ) : dogData.error ? (
-        <h2>{dogData.error}</h2>
-    ): isLoggedIn ?(<DoggyDexToggle dogData={dogData}/>) : <LearningModeDoggyDex dogData={dogData}/>
-
+  return dogData.loading ? (
+    <h2>Loading</h2>
+  ) : dogData.error ? (
+    <h2>{dogData.error}</h2>
+  ) : isLoggedIn ? (
+    <DoggyDexToggle dogData={dogData} />
+  ) : (
+    <LearningModeDoggyDex dogData={dogData} />
+  );
 }
 
-    const mapStateToProps = state => { 
-        return {
-            dogData: state.dogReducer
-        };
-    };
+const mapStateToProps = (state) => {
+  return {
+    dogData: state.dogReducer,
+  };
+};
 
-    const mapDispatchToProps = dispatch => {
-        return {
-            fetchDogs: () => dispatch(fetchDogs())
-        };
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchDogs: () => dispatch(fetchDogs()),
+  };
+};
 
-
-
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(DoggyDex) 
-
-
+export default connect(mapStateToProps, mapDispatchToProps)(DoggyDex);
