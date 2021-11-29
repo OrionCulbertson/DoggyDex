559d05971f918c0a40b286eae21f81ec2826e12f
mgacoder <miguelgalvanalvarado@gmail.com>
2021-11-28 16:19:47 -0800

    added logo inside ddlm and ddp components

--- application/client-end/src/components/LearningModeDoggyDex.js
+++ application/client-end/src/components/LearningModeDoggyDex.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Logo from './Logo'
 
 const LearningModeDoggyDex = ({ dogData }) => {
   const history = useHistory();
   const handleClick = (dogs) => {
-    console.log('inside lmdd', dogs);
-    history.push('/doginfo', { dogs: dogs });
-  };
+    history.push('/doginfo', {dogs: dogs});
+  }
 
-    const history = useHistory();
-    const handleClick = (dogs) => {
-        history.push('/doginfo', {dogs: dogs});
-   }
-
-    return (
-        <div className ="doggyDex-container">
-                 {dogData &&
-                dogData.dogs &&
-                dogData.dogs.map(dogs => <button id='dogs' onClick={() => handleClick(dogs)}><text className="doggyDex-text">{dogs.dogbreed}</text><img className="dogImg" src={dogs.img}></img></button>)}
-        </div>
-    )
+  return (
+    <>
+      <Logo/>
+      <div className ="doggyDex-container">
+                {dogData &&
+              dogData.dogs &&
+              dogData.dogs.map(dogs => <button id='dogs' onClick={() => handleClick(dogs)}><text className="doggyDex-text">{dogs.dogbreed}</text><img className="dogImg" src={dogs.img}></img></button>)}
+      </div>
+    </>
+  )
 }
 
 export default LearningModeDoggyDex
