eb8601574e1df223f2a54383f085f7152f2a6ad4
OrionCulbertson <oculbertson@mail.sfsu.edu>
2021-12-08 10:49:47 -0800

    Added: Recognized Dog Breed Gets Logged in User's DoggyDex, Notification for when recognized dog breed isn't in the Dog Breed database

---
+++ application/client-end/src/components/DogNotSupported.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DogNotSupported = () => {
+    return (
+        <p className="dogUnsupported">
+            We don't currently have this dog breed in 
+            our DoggyDex system.
+        </p>
+    )
+}
+
+export default DogNotSupported
